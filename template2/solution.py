936f2bf5b9d3cf03f6f0f0209892659b5d5a5c9f
Ethan Christensen <etc@unknownstudio.com>
2024-11-30 22:19:35 -0700

    feat(template2): auto-copy answer

--- template2/solution.py
+++ template2/solution.py
@@ -2,6 +2,7 @@ import sys
 import re
 from functools import reduce
 import math
+import pyperclip
 
 input = open(sys.argv[1]).read().strip()
 
@@ -16,3 +17,7 @@ p2 = 0
 
 print("Part 1:", p1)
 print("Part 2:", p2)
+
+to_copy = p1 if p2 == 0 else p2
+pyperclip.copy(to_copy)
+print(f"Copied {"P1" if p2 == 0 else "P2"}: \"{to_copy}\"")
