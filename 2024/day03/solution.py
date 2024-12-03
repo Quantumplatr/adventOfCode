ae1a55d0c042c1fab80cae772d32b4b6738b8381
Ethan Christensen <etc@unknownstudio.com>
2024-12-02 22:23:43 -0700

    2024 Day 03

---
+++ 2024/day03/solution.py
@@ -0,0 +1,73 @@
+import sys
+import re
+from functools import reduce
+import math
+import pyperclip
+
+# Solved: 12/2/2024
+#
+#        | Time     | Rank
+# -------+----------+-----
+# Part 1 | 00:02:16 |  289
+# Part 2 | 00:12:59 | 1792
+#
+# Things to improve:
+# - I'm actually pretty proud of my part 1.
+#   Idk what I would've improved. Maybe the regex so I don't findall twice
+#   and go straight to an array of pairs. I assume that's possible.
+# - Part 2 I started with trying to use regex. That was a mistake b/c I'm
+#   not THAT familiar with regex for lookaheads/behinds.
+# - Could've been a little faster/cleaner with my part 2 just in execution
+#   but I'm pretty happy w/ my perfomance
+
+input = open(sys.argv[1]).read().strip()
+
+p1 = 0
+p2 = 0
+
+# ---- CODE HERE ---- #
+
+# Get all instances of "mul(<number>,<number>)"
+muls = re.findall(r"(mul\(\d+,\d+\))", input)
+
+# Calculate sum of a*b for each a,b pair
+for mul in muls:
+    a, b = re.findall(r"(\d+)", mul)
+    p1 += int(a) * int(b)
+
+# Do the same as above but only if there was a `do()` or `start of line`
+# more recent than a `don't()`
+i = 0
+can_mult = True
+# Iterate through input
+while i < len(input):
+
+    # Get substring
+    curr = input[i:]
+    
+    # Check if we're at a do or don't
+    if curr.startswith("don't()"):
+        can_mult = False
+    if curr.startswith("do()"):
+        can_mult = True
+
+    # If can mult, check for a mult.
+    if can_mult:
+        # Initially did a findall. After solving, I changed it to match.
+        match = re.match(r"^mul\((\d+),(\d+)\)", curr)
+        if match:
+            a,b = match.group(1,2)
+
+            if mul:
+                p2 += int(a) * int(b)
+
+    i += 1
+
+# ------------------- #
+
+print("Part 1:", p1)
+print("Part 2:", p2)
+
+to_copy = p1 if p2 == 0 else p2
+pyperclip.copy(to_copy)
+print(f"Copied {'P1' if p2 == 0 else 'P2'}: \"{to_copy}\"")
